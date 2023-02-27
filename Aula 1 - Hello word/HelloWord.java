8521f1ae19c8b6d7868a197754ad4ee0c2602c5b
luisa spinola <luisa.spinola@cubos.academy>
2023-02-27 11:27:00 +0000

    first lecture

---
+++ Aula 1 - Hello word/HelloWord.java
@@ -0,0 +1,13 @@
+public class HelloWord{
+    public static void main(String[] args){
+
+        int num=100;
+        String name="Luisa";
+
+        System.out.print("CFB Cursos\n");
+        System.out.println("Hello world!");
+        System.out.printf("%s - %d%n", "Hello world!", 2023);
+        System.out.printf("Valor da variavel num:%d%n", num);
+        System.out.printf("Valor da variavel name:%s%n", name);
+    }
+}
